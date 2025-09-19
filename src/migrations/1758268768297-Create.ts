4953de5d3275c819ec6367de5d5aff97173cc75f
kurushiba <kurushiba55555@gmail.com>
2025-09-19 16:59:53 +0900

    modify migration

--- src/migrations/1758268768297-Create.ts
+++ src/migrations/1758268768297-Create.ts
@@ -1,14 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateUser1757292423766 implements MigrationInterface {
-    name = 'CreateUser1757292423766'
+export class Create1758268768297 implements MigrationInterface {
+    name = 'Create1758268768297'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "thumbnailUrl" varchar, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')))`);
         await queryRunner.query(`CREATE UNIQUE INDEX "IDX_e12875dfb3b1d92d7d7c5377e2" ON "user" ("email") `);
+        await queryRunner.query(`CREATE TABLE "meeting" ("id" varchar PRIMARY KEY NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "meeting"`);
         await queryRunner.query(`DROP INDEX "IDX_e12875dfb3b1d92d7d7c5377e2"`);
         await queryRunner.query(`DROP TABLE "user"`);
     }
